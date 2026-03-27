70364bb08c0cb31635aa67dcc5c5fe9571c86056
Nandini <nandunandu10142@gmail.com>
2026-03-27 12:14:44 +0530

    initial FastAPI project

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ai-interview-chatbot
+# FastAPI Interview question generator
+ This is simple FastAPI project that generates interview questions based on topic
+
+ ## features
+ -generate questions for java, python,ect.
+ -REST API using FastAPI
+ -Swagger UI Support
+
+ ## How to run
+
+ '''bash
+ pip install -r requirements.txt
+ uvicorn main:app --reload
+
---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+questions = {
+    "java": [
+        "what is OOP?",
+        "what is JVM?",
+        "explain inheritance in java?"
+    ],
+    "python": [
+        "what is a list?",
+        "what is dictionary?"
+    ]
+}
+
+class UserInput(BaseModel):
+    topic: Optional[str] = None
+    answer: Optional[str] = None
+
+
+@app.get("/")
+def home():
+    return {"message": "API is running"}
+
+
+@app.post("/get-question")
+def get_question(data: UserInput):
+    if not data.topic:
+        return {"message": "please provide a topic"}
+
+    topic = data.topic.lower()
+
+    if topic in questions:
+        return {"questions": questions[topic]}
+
+    return {"message": "topic not found"}
+
+
+@app.post("/evaluate")
+def evaluate(data: UserInput):
+    if not data.answer:
+        return {"feedback": "please provide an answer"}
+
+    answer = data.answer.strip()
+
+    if len(answer) < 20:
+        return {"feedback": "try explaining in more detail"}
+
+    return {"feedback": "good answer, try adding examples for improvement"}
